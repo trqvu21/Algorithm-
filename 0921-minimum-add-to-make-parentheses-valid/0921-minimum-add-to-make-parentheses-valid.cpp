43e524d8ef717aefa697b8a15233dd4678614167
trqvu21 <tqv4560@gmail.com>
2026-04-07 09:49:42 +0700

    Time: 0 ms (100%), Space: 8.4 MB (82.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/?envType=problem-list-v2&envId=w5zahxti
 class Solution {
 public:
     int minAddToMakeValid(string s) {
