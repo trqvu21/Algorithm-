5042c0722aa8881d24021f897a57c4fae2c87d75
Truong Quang Vu <tqv4560@gmail.com>
2026-06-11 23:11:35 +0700

    Time: 0 ms (100%), Space: 8.5 MB (91.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        long long rev = 0;
+        int original = x;
+
+        while (x > 0) {
+
+            int digit = x % 10;
+
+            rev = rev * 10 + digit;
+
+            x /= 10;
+        }
+
+        return rev == original;
+    }
+};
